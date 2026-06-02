0ca85a65150791bbc91211d577eadab88e15fc8f
2312121 <23121@burnside.school.nz>
2026-06-03 10:26:32 +1200

    Trying to connect database to python and display player cards in player page

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
-from flask import Flask, render_template
+import sqlite3
+
+from flask import Flask, render_template, g
+
+DATABASE = 'database.db'
 
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 #Links my python to my home html
 @app.route("/")
 def home():
     return render_template('home.html')
 
+#links my python to my players page
 @app.route("/players")
 def players():
+    #Do query get results back send to template
     return render_template("players.html")
 
+#links my team page to my python 
 @app.route("/teams")
 def teams():
     return render_template("teams.html")
+
 
 
 if __name__ == "__main__":
